ee33a87c71f41edddb9d54dd692bbd86a8caef42
vade <vade@vade.info>
2017-04-16 19:14:28 -0400

    New project.

---
+++ Synergy/Synergy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Synergy
+//
+//  Created by vade on 4/16/17.
+//  Copyright © 2017 Synopsis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
